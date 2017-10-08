9a938ca65fca080d9a8b763c478d512661b71704
Gustavo S. Coelho <gustavo.coelho@totemti.com.br>
2017-10-08 13:31:15 -0400

    Criação da página de login
    Importado o materialize.css para a composição do layout

--- src/routes.php
+++ src/routes.php
@@ -5,10 +5,20 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->get('/', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get('/login', function (Request $request, Response $response, array $args) {
+    //Log de mensagens
+    $this->logger->info("Mystic-Labs '/login' route");
+    
+    
+    
+    //Render login view
+    return $this->renderer->render($response, 'login.phtml', $args);
 });
